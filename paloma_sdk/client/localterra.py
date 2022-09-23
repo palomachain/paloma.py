6059182a35f3671a826dc13a322a008719d2ef4e
Jason Jacobs <nerfnerd@gmail.com>
2022-09-23 15:41:07 -0700

    ALL CAPPS

--- paloma_sdk/client/localterra.py
+++ paloma_sdk/client/localterra.py
@@ -4,9 +4,9 @@ from paloma_sdk.key.mnemonic import MnemonicKey
 
 from .lcd import AsyncLCDClient, AsyncWallet, LCDClient, Wallet
 
-__all__ = ["LOCALTERRA_MNEMONICS", "LocalPaloma", "AsyncLocalPaloma"]
+__all__ = ["LOCALPALOMA_MNEMONICS", "LocalPaloma", "AsyncLocalPaloma"]
 
-LOCALTERRA_MNEMONICS = {
+LOCALPALOMA_MNEMONICS = {
     "validator": "satisfy adjust timber high purchase tuition stool faith fine install that you unaware feed domain license impose boss human eager hat rent enjoy dawn",
     "test1": "notice oak worry limit wrap speak medal online prefer cluster roof addict wrist behave treat actual wasp year salad speed social layer crew genius",
     "test2": "quality vacuum heart guard buzz spike sight swarm shove special gym robust assume sudden deposit grid alcohol choice devote leader tilt noodle tide penalty",
@@ -20,7 +20,7 @@ LOCALTERRA_MNEMONICS = {
     "test10": "prefer forget visit mistake mixture feel eyebrow autumn shop pair address airport diesel street pass vague innocent poem method awful require hurry unhappy shoulder",
 }
 
-LOCALTERRA_DEFAULTS = {
+LOCALPALOMA_DEFAULTS = {
     "url": "http://localhost:1317",
     "chain_id": "localpaloma",
     "gas_prices": {"uluna": "0.15"},
@@ -37,13 +37,13 @@ class AsyncLocalPaloma(AsyncLCDClient):
     """Ready-to use :class:`Wallet` objects with LocalPaloma default accounts."""
 
     def __init__(self, *args, **kwargs):
-        options = {**LOCALTERRA_DEFAULTS, **kwargs}
+        options = {**LOCALPALOMA_DEFAULTS, **kwargs}
         super().__init__(*args, **options)
         self.wallets = {
             wallet_name: self.wallet(
-                MnemonicKey(mnemonic=LOCALTERRA_MNEMONICS[wallet_name])
+                MnemonicKey(mnemonic=LOCALPALOMA_MNEMONICS[wallet_name])
             )
-            for wallet_name in LOCALTERRA_MNEMONICS
+            for wallet_name in LOCALPALOMA_MNEMONICS
         }
 
 
@@ -61,11 +61,11 @@ class LocalPaloma(LCDClient):
     """
 
     def __init__(self, *args, **kwargs):
-        options = {**LOCALTERRA_DEFAULTS, **kwargs}
+        options = {**LOCALPALOMA_DEFAULTS, **kwargs}
         super().__init__(*args, **options)
         self.wallets = {
             wallet_name: self.wallet(
-                MnemonicKey(mnemonic=LOCALTERRA_MNEMONICS[wallet_name])
+                MnemonicKey(mnemonic=LOCALPALOMA_MNEMONICS[wallet_name])
             )
-            for wallet_name in LOCALTERRA_MNEMONICS
+            for wallet_name in LOCALPALOMA_MNEMONICS
         }
